06f5461ec809c21007ec0fd847cbeaf6245e6874
DariaSydorenko <104034908+DariaSydorenko@users.noreply.github.com>
2023-06-02 22:56:57 +0300

    Create Hit.php

---
+++ app/Models/Hit.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hit extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'phone_id';
+
+    public function model()
+    {
+        return $this->belongsTo(PhoneModel::class, 'model_id', 'model_id');
+    }
+
+    public function color()
+    {
+        return $this->belongsTo(PhoneColor::class, 'color_id', 'color_id');
+    }
+}
